859f91cbf42b051ec3184a7a892fd9eb0295c684
Janne Hellsten <jjhellst@gmail.com>
2018-06-11 01:10:09 +0300

    bad line and normal line timing tests

--- protos/raster-timing-test/macros.asm
+++ protos/raster-timing-test/macros.asm
@@ -1,3 +1,24 @@
+//----------------------------------------------------------
+// Code for creating the breakpoint file sent to Vice.
+//----------------------------------------------------------
+.var _useBinFolderForBreakpoints = cmdLineVars.get("usebin") == "true"
+.var _createDebugFiles = cmdLineVars.get("afo") == "true"
+.print "File creation " + [_createDebugFiles
+ ? "enabled (creating breakpoint file)"
+ : "disabled (no breakpoint file created)"]
+.var brkFile
+.if(_createDebugFiles) {
+ .if(_useBinFolderForBreakpoints)
+ .eval brkFile = createFile("bin/breakpoints.txt")
+ else
+ .eval brkFile = createFile("breakpoints.txt")
+}
+.macro break() {
+.if(_createDebugFiles) {
+ .eval brkFile.writeln("break " + toHexString(*))
+ }
+}
+
 .macro DebugRaster(color) {
     .if (debug) {
         pha
--- protos/raster-timing-test/raster-timing.asm
+++ protos/raster-timing-test/raster-timing.asm
@@ -1,4 +1,5 @@
 
+.const badline = false
 .const debug = true
 :BasicUpstart2(start)
 
@@ -19,19 +20,14 @@
 //----------------------------------------------------------
 //              Variables
 //----------------------------------------------------------
-.const irq0line = 16
+.var irq0line = 52+8
+.if (badline) {
+    .eval irq0line = 52+8-4
+}
 
 start: {
     ClearScreen($0400, $20)
     ClearScreen($d800, LIGHT_BLUE)
-
-    lda #$a0
-    .for (var y = 16; y < 25; y++) {
-        .for (var x = 0; x < 40; x++) {
-            sta $0400 + y*40 + x
-
-        }
-    }
 
     lda #0
     sta framecount
@@ -50,7 +46,6 @@ start: {
     sta framecount
     cli
 
-
 infloop:
     lda framecount
 vsync:
@@ -62,8 +57,106 @@ vsync:
 
 framecount: .byte 0
 
+.align 256
 irq0: {
-    irq_start(end)
+    double_irq(end, irq1)
+irq1:
+    txs
+    ldx #$08
+    dex
+    bne *-1
+    bit $00
+
+    // Check if $d012 is incremented and rectify with an aditional cycle if neccessary
+    lda $d012
+    cmp $d012  // <- critical instruction (ZERO-Flag will indicate if Jitter = 0 or 1)
+
+    // CYCLECOUNT: [61 -> 62] <- Will not work if this timing is wrong
+
+    // cmp $d012 is originally a 5 cycle instruction but due to piplining tech. the
+    // 5th cycle responsible for calculating the result is executed simultaniously
+    // with the next OP fetch cycle (first cycle of beq *+2).
+
+    // Add one cycle if $d012 wasn't incremented (Jitter / ZERO-Flag = 0)
+    beq *+2
+
+    /////////////////////////////////////////////////////////////
+    // now at clock 3
+    // http://www.lemon64.com/forum/viewtopic.php?t=64250&sid=6b7d7fbfc941f116cb7ca8247b1d748d
+    /////////////////////////////////////////////////////////////
+
+.if (!badline) {
+    // VICE says this break happens on cycle 3 of the scanline
+    break()
+    // 0
+    nop
+    nop
+    nop
+    nop
+    // 8
+    // Scanline's first char (at maybe clock 12?)
+    inc $d021
+    inc $d021
+    inc $d021
+    inc $d021
+    inc $d021
+    inc $d021
+    // 36 + 8 = 44
+
+    lda #0
+    sta $d021
+    // 50
+
+    bit $fe
+    // 53
+    nop
+    nop
+    nop
+    nop
+    nop
+    // 63
+
+    // next scanline
+    // skip border
+    nop
+    nop
+    nop
+    nop
+    inc $d021
+    inc $d021
+
+    lda #0
+    sta $d021
+} else {
+    break()
+
+    .for (var i = 0; i < 30; i++) {
+        nop
+    }
+
+    // this should be clock 0 of bad line now
+
+    // do 20 cycles of read-only work
+    .for (var i = 0; i < 10; i++) {
+        nop
+    }
+    // now we're at clock 0 of normal line
+
+    // skip enough to get the first color write to hit before raster beam gets there
+    bit $fe
+    nop
+    nop
+    nop
+    nop
+
+    // boom we lost 40 cycles here
+    inc $d021
+    inc $d021
+    // 48
+    lda #0
+    sta $d021
+    // 54
+}
 
     inc framecount
 
